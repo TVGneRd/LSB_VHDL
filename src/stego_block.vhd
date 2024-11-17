5d5b94fc637f1127541d0b72999984c12d611d9a
Andrey <andres.fomin2803@gmail.com>
2024-11-18 00:04:35 +0300

    Первая пробная версия блока

--- src/stego_block.vhd
+++ src/stego_block.vhd
@@ -33,18 +33,32 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity stego_block is
     port (
-        clk         : in  std_logic;
-        reset       : in  std_logic;
-        data_in     : in  std_logic_vector(23 downto 0);
-        data_valid  : in  std_logic;
-        data_ready  : out std_logic;
-        data_out    : out std_logic_vector(23 downto 0)
+        clk          : in  std_logic;
+        reset        : in  std_logic;
+        pixel_in     : in  std_logic_vector(23 downto 0);
+        pixel_valid  : in  std_logic;
+        pixel_ready  : out std_logic;
+        msg_in       : in std_logic;
+        pixel_out    : out std_logic_vector(23 downto 0)
     );
 end stego_block;
 
 architecture Behavioral of stego_block is
-
+    --signal data_counter : integer := 0;
+    --signal pixel_buffer : STD_LOGIC_VECTOR(23 downto 0);
+    signal done_flag : STD_LOGIC := '0';
 begin
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            done_flag <= '0';
+        elsif rising_edge(clk) then
+            pixel_out <= pixel_in(23 downto 1) & msg_in;  
+            done_flag = '1';
+        end if;
+    end process;
 
+    pixel_ready <= done_flag;
+    pixel_out <= pixel_out;
 
 end Behavioral;
